5dc57ee8b98f38a9d367889dcd1f632f531e8a1f
kefeh <kefehcollins@gmail.com>
2019-06-05 20:20:50 +0100

    Type: Feature, Body: Add the function that posts the notification to slack

---
+++ slack_notifications.py
@@ -0,0 +1,26 @@
+import requests
+
+
+kefeh_url = "https://hooks.slack.com/services/T2someCharactersForNotiff"
+
+
+def post_to_channel(webhook_url: str, message: str):
+
+    """
+    Function sends a slack notification to a channel or user
+    :param webhook_url: The web hook url of the user or the channel
+    :param message:The message that should be sent to user
+    :return:
+    """
+
+    if True:  # should_send_slack_msg:
+        try:
+            response = requests.post(url=webhook_url, json={'text': message})
+            if response.status_code != 200:
+                return "Failed to send message. Status code:{}".format(response.status_code)
+
+            else:
+                return "Message successfully sent to channel"
+
+        except Exception as ex:
+            return "Failed to send message because of {}".format(ex)
